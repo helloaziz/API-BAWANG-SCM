4e9d5e5d65fb26a81caea71ef08d339613b957a5
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-29 11:47:10 +0700

    make model for db_bawang

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    wallet_address: DataTypes.STRING,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    location: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
