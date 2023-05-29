d4e743d6871f5716b15b9bd7d037cd36418fcd52
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-29 11:47:43 +0700

    make seeder for role

---
+++ db/seeders/20230529043800-demo-roles.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Roles",
+      [
+        {
+          id: 1,
+          name: "ADMIN",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: "PETANI",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          name: "PENGEPUL",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Roles", null, {});
+  },
+};
