bda6652249420551904a1a754a99e6a3c05dd4b8
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-06 09:50:23 +0700

    add routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const auth = require("../controllers/auth");
+const authorize = require("../middlewares/authorize");
 
 router.post("/register", auth.registerBuyer);
 router.post("/login", auth.login);
-router.get("/whoami", auth.whoami);
+router.get("/whoami", authorize(), auth.whoami);
+router.put("/change-password", authorize(), auth.changePassword);
+router.put("/forgot-password", auth.forgotPassword);
+router.put("/reset-password", auth.resetPassword);
 
 module.exports = router;
