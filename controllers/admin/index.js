e47879e6816047c6559e0eb33bf5df1339731e7d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-06 07:12:59 +0700

    modify controllers admin

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,9 +1,12 @@
-const { User } = require("../../models");
-const { BadRequestError } = require("../../errors");
+const { User, Role } = require("../../models");
+const { BadRequestError, NotFoundError } = require("../../errors");
+const { ROLES } = require("../../utils/enum");
 const bcrypt = require("bcrypt");
 const Validator = require("fastest-validator");
 const { StatusCodes } = require("http-status-codes");
 const v = new Validator();
+const { PASSWORD_DEFAULT } = process.env;
+const { Op } = require("sequelize");
 
 const registerPatner = async (req, res, next) => {
   try {
@@ -19,14 +22,12 @@ const registerPatner = async (req, res, next) => {
     } = req.body;
     const schema = {
       email: { type: "email", label: "Email Address" },
-      password: { type: "string", min: 6 },
     };
 
     const check = await v.compile(schema);
 
     const validate = check({
       email: `${email}`,
-      password: `${password}`,
     });
 
     if (validate.length > 0) {
@@ -35,17 +36,34 @@ const registerPatner = async (req, res, next) => {
       );
     }
 
-    if (password !== confirm_password) {
-      throw new BadRequestError(`Password doesn't match`);
-    }
-
-    const userExist = await User.findOne({ where: { email, phone_number } });
+    const userExist = await User.findOne({
+      where: {
+        [Op.or]: [{ email }, { phone_number }],
+      },
+    });
 
     if (userExist) {
-      throw new BadRequestError("User sudah terdaftar!");
+      throw new BadRequestError("Email/Nomer telepon sudah terdaftar!");
     }
 
-    const passwordHashed = await bcrypt.hash("12345678", 10);
+    const checkRoles = await Role.findOne({ where: { id: role_id } });
+    if (!checkRoles) {
+      throw new NotFoundError("Role tidak tersedia!");
+    }
+    console.log(checkRoles.name);
+    console.log(ROLES.PETANI);
+    if (
+      checkRoles.name !== ROLES.PETANI &&
+      checkRoles.name !== ROLES.PENGEPUL
+    ) {
+      throw new BadRequestError(
+        "Hanya role Petani & Pengepul yang dibolehkan!"
+      );
+    }
+
+    const password = PASSWORD_DEFAULT;
+
+    const passwordHashed = await bcrypt.hash(password, 10);
 
     const result = await User.create({
       wallet_address,
