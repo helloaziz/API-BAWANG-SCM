135a67c20df24d14a469fde079f546fe4008c0bb
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-13 12:15:02 +0700

    modify auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,8 @@ router.post("/register", auth.registerBuyer);
 router.post("/login", auth.login);
 router.get("/whoami", authorize(), auth.whoami);
 router.put("/change-password", authorize(), auth.changePassword);
-router.put("/forgot-password", auth.forgotPassword);
+router.post("/forgot-password", auth.forgotPassword);
 router.put("/reset-password", auth.resetPassword);
+router.put("/activate-account", authorize(), auth.activateAccount);
 
 module.exports = router;
