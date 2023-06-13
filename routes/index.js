65ff0efc675c50ea72175fd331478665ef26b168
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-13 17:25:33 +0700

    modify index routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const role = require("./role");
 const auth = require("./auth");
 const admin = require("./admin");
 const product = require("./product");
+const user = require("./user-profile");
 
 router.get("/", (req, res) => {
   return res.status(200).json({
@@ -16,5 +17,6 @@ router.use("/role", role);
 router.use("/auth", auth);
 router.use("/admin", admin);
 router.use("/product", product);
+router.use("/user", user);
 
 module.exports = router;
