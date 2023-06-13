23808e78849a12ceddbacb21566700b9b4e601be
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-13 17:25:16 +0700

    modify admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,10 @@ const admin = require("../controllers/admin");
 const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
-router.post("/register-patner", authorize(ROLES.ADMIN), admin.registerPatner);
-router.post("/register-admin", admin.registerAdmin);
+router.post("/registerPatner", authorize(ROLES.ADMIN), admin.registerPatner);
+router.get("/userIndex", authorize(ROLES.ADMIN), admin.index);
+router.get("/userShow/:user_id", authorize(ROLES.ADMIN), admin.show);
+router.put("/userUpdate/:user_id", authorize(ROLES.ADMIN), admin.update);
+router.post("/registerAdmin", admin.registerAdmin);
 
 module.exports = router;
