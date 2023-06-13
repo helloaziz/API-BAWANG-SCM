94238b00e89844382eb23d15ad436e8b2adc91b6
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-13 12:13:56 +0700

    modify seeder role

--- db/seeders/20230529043800-demo-roles.js
+++ db/seeders/20230529043800-demo-roles.js
@@ -6,31 +6,26 @@ module.exports = {
       "Roles",
       [
         {
-          id: 1,
           name: "ADMIN",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 2,
           name: "PETANI",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 3,
           name: "PENGEPUL",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 4,
           name: "RETAILER",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 5,
           name: "BUYER",
           createdAt: new Date(),
           updatedAt: new Date(),
