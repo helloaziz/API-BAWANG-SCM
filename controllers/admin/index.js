a51b2a1ccde034122b9a60619fdf0bf2bbd8a842
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-13 17:24:49 +0700

    modify controller admin

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,6 +1,6 @@
 const { User, Role } = require("../../models");
 const { BadRequestError, NotFoundError } = require("../../errors");
-const { ROLES } = require("../../utils/enum");
+const { ROLES, EMAIL_STATUS } = require("../../utils/enum");
 const bcrypt = require("bcrypt");
 const Validator = require("fastest-validator");
 const { StatusCodes } = require("http-status-codes");
@@ -20,8 +20,9 @@ const registerPatner = async (req, res, next) => {
       phone_number,
       role_id,
     } = req.body;
+
     const schema = {
-      email: { type: "email", label: "Email Address" },
+      email: { type: "email" },
     };
 
     const check = await v.compile(schema);
@@ -93,6 +94,7 @@ const registerPatner = async (req, res, next) => {
       phone_number,
       password: passwordHashed,
       role_id,
+      status: EMAIL_STATUS.ACTIVE,
     });
 
     return res.status(StatusCodes.OK).json({
@@ -171,4 +173,143 @@ const registerAdmin = async (req, res, next) => {
   }
 };
 
-module.exports = { registerPatner, registerAdmin };
+const index = async (req, res, next) => {
+  try {
+    const { email, phone_number } = req.query;
+
+    let where = {};
+
+    if (email) {
+      where = {
+        email: email,
+      };
+    } else if (phone_number) {
+      where = {
+        phone_number: phone_number,
+      };
+    }
+
+    const result = await User.findAll({
+      where,
+      include: [{ model: Role, as: "role" }],
+    });
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get users",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const show = async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+
+    const result = await User.findOne({
+      where: { id: user_id },
+      include: [{ model: Role, as: "role" }],
+    });
+
+    if (!result) {
+      throw new NotFoundError("User tidak ditemukan!");
+    }
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get user",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const {
+      first_name,
+      last_name,
+      email,
+      location,
+      address,
+      phone_number,
+      role_id,
+    } = req.body;
+
+    const schema = {
+      email: { type: "email" },
+    };
+
+    const check = await v.compile(schema);
+
+    const validate = check({
+      email: `${email}`,
+    });
+
+    if (validate.length > 0) {
+      throw new BadRequestError("Email tidak valid!");
+    }
+
+    const phoneSchema = {
+      phone_number: {
+        type: "string",
+        pattern: /^0\d{9,12}$/,
+        max: 13,
+      },
+    };
+
+    const checkPhone = await v.compile(phoneSchema);
+
+    const validatePhone = checkPhone({
+      phone_number: `${phone_number}`,
+    });
+
+    if (validatePhone.length > 0) {
+      throw new BadRequestError("Format nomor telepon salah!");
+    }
+
+    const checkUser = await User.findOne({ where: { id: user_id } });
+    if (!checkUser) {
+      throw new NotFoundError("User tidak ada!");
+    }
+
+    const checkMobile = await User.findOne({
+      where: { phone_number, id: { [Op.ne]: user_id } },
+    });
+
+    const checkEmail = await User.findOne({
+      where: { email, id: { [Op.ne]: user_id } },
+    });
+
+    if (checkMobile || checkEmail) {
+      throw new BadRequestError("Email/nomer telepon sudah terdaftar");
+    }
+
+    const result = await User.update(
+      {
+        first_name,
+        last_name,
+        email,
+        location,
+        address,
+        phone_number,
+        role_id,
+      },
+      { where: { id: user_id } }
+    );
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success update user",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { registerPatner, registerAdmin, index, show, update };
